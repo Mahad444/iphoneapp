476702333b65c89f71efef825a479ce76c2ec641
Ally Juma Said <mahadsaid95@icloud.com>
2023-09-01 01:11:08 +0100

    start learning Swift

---
+++ ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Fua-nguo
+//
+//  Created by apple on 31/08/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world! By Mahad Said")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Fua_nguoApp.swift
@@ -0,0 +1,17 @@
+//
+//  Fua_nguoApp.swift
+//  Fua-nguo
+//
+//  Created by apple on 31/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct Fua_nguoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
